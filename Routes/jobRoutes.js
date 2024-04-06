237548288937c0167977ae4b498110cd3b50d2b3
Bozhidar Kumanov <67313433+bojo509@users.noreply.github.com>
2024-04-06 23:15:00 +0300

    Updated job routes

--- Routes/jobRoutes.js
+++ Routes/jobRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 //JWT required
 router.post("/upload-job", userAuth, createJob);
 router.put("/update-job/:id", userAuth, updateJob);
-router.get("/get-all", getJobPosts)
+router.get("/find-jobs", getJobPosts)
 router.get("/get-job-details/:id", getJobById)
 router.delete("/delete-job/:id", deleteJobPost)
 
